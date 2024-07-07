ec543ed966e49d7f8c201f24636f219ef1b5455e
YoungMayor <youngmayor.dev@gmail.com>
2024-07-07 10:18:41 +0100

    Changed theme color

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+        ),
         useMaterial3: true,
       ),
       home: home,
