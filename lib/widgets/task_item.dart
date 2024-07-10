ae35394b204e885da9727c6a23d34e48c8ed3fe1
YoungMayor <youngmayor.dev@gmail.com>
2024-07-11 00:08:15 +0100

    Update task_item.dart

--- lib/widgets/task_item.dart
+++ lib/widgets/task_item.dart
@@ -1,5 +1,4 @@
 import 'package:drift/drift.dart' as drift;
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoman/database.dart';
