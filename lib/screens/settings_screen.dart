645753d44ea0387d30f4617a76988feb0cc1c9e1
YoungMayor <youngmayor.dev@gmail.com>
2024-07-12 16:54:43 +0100

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -3,16 +3,34 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:todoman/screens/settings/about_us_screen.dart';
 import 'package:todoman/screens/settings/delete_data_screen.dart';
 import 'package:todoman/screens/settings/faq_screen.dart';
-// import 'package:todoman/screens/settings/help_and_feedback_screen.dart';
+
+void _pushPage(BuildContext context, Widget page) {
+  Navigator.of(context).push(MaterialPageRoute(
+    builder: (context) => page,
+  ));
+}
+
+typedef MenuItemCallback = void Function(BuildContext);
 
 class SettingsScreen extends StatelessWidget {
   SettingsScreen({super.key});
 
-  final List<(String, IconData, Widget)> settingMenuItems = [
-    ("About us", Icons.info_outline, const AboutUsScreen()),
-    ("Frequently Asked Questions", Icons.question_mark, FAQScreen()),
-    // ("Help & Feedback", Icons.feedback_outlined, const HelpAndFeedbackScreen()),
-    ("Delete Data", Icons.delete_outline_rounded, const DeleteDataScreen()),
+  final List<(String, IconData, MenuItemCallback)> settingMenuItems = [
+    (
+      "About us",
+      Icons.info_outline,
+      (context) => _pushPage(context, const AboutUsScreen())
+    ),
+    (
+      "Frequently Asked Questions",
+      Icons.question_mark,
+      (context) => _pushPage(context, FAQScreen())
+    ),
+    (
+      "Delete Data",
+      Icons.delete_outline_rounded,
+      (context) => _pushPage(context, const DeleteDataScreen())
+    )
   ];
 
   @override
@@ -34,18 +52,14 @@ class SettingsScreen extends StatelessWidget {
             child: ListView.separated(
               itemCount: settingMenuItems.length,
               itemBuilder: (context, index) {
-                (String, IconData, Widget) settingItem =
+                (String, IconData, MenuItemCallback) settingItem =
                     settingMenuItems[index];
 
                 return ListTile(
                   title: Text(settingItem.$1),
                   leading: Icon(settingItem.$2),
                   trailing: const Icon(Icons.chevron_right),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => settingItem.$3,
-                    ));
-                  },
+                  onTap: () => settingItem.$3(context),
                 );
               },
               separatorBuilder: (context, index) => const Divider(
